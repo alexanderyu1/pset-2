8e96dad4fc72f03e3733df5f59c90d88f9d8789a
Alexander Yu <ayu1@ucvts.org>
2019-10-23 12:31:51 -0400

    first commit

--- src/perimeter.js
+++ src/perimeter.js
@@ -1,14 +1,14 @@
 const readlineSync = require("readline-sync");
 const width = readlineSync.question("Width: ");
 const length = readlineSync.question("Length: ");
-const Conversion = 25.4;
+const Conversion = 2.54;
 
 let x = length;
 let y = width;
 let a = x*Conversion;
 let b = y*Conversion;
-let c = a*b;
+let c = (2*a)+(2*b);
 let r = Math.round (c);
 c = c.toLocaleString ("en", {minimumFractionDigits: 2, maximumFractionDigits: 2} );
 
-console.log("A(n) " + y + "-by-" + x + "-inch sheet of paper has an area of " + c + " square millimiter(s).");
+console.log("A(n) " + y + "-by-" + x + "-inch sheet of paper has a perimeter of " + c + " centimeter(s).");
