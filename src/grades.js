36a558db44538ffa7a562b52d7823144e8494112
Alexander Yu <ayu1@ucvts.org>
2019-10-25 11:57:38 -0400

    fourth commit

--- src/grades.js
+++ src/grades.js
@@ -11,9 +11,13 @@ const tests1 = readlineSync.question("Enter three test grades.\n" );
 const tests2 = readlineSync.question("" );
 const tests3 = readlineSync.question("" );
 
-let h = homework1 + homework2 + homework3;
-let q = quizzes1 + quizzes2 + quizzes3;
-let t = tests1 + tests2 + tests3;
-let c = ((0.15*h) + (0.35*q) + (0.5*t)) / 9 ;
+let h = ((Number(homework1)) + (Number(homework2)) + (Number(homework3)));
+let q = ((Number(quizzes1)) + (Number(quizzes2)) + (Number(quizzes3)));
+let t = ((Number(tests1)) + (Number(tests2)) + (Number(tests3)));
 
-console.log("Your marking period grade is " + c);
+let c = ((0.15*h) + (0.35*q) + (0.5*t)) / 3 ;
+
+let r = Math.round (c);
+c = c.toLocaleString ("en", {minimumFractionDigits: 2, maximumFractionDigits: 2} );
+
+console.log("Your marking period grade is " + c + "%.");
