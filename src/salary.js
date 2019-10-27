9b5dadd66fcf23b925dbe266e9de392337aaa246
Alex Yu <alex@NEBULA.milkyway.net>
2019-10-27 18:23:45 -0400

    nth commit

--- src/salary.js
+++ src/salary.js
@@ -1,16 +1,15 @@
 const readlineSync = require("readline-sync");
 const annual = readlineSync.question("Annual Salary: ");
 
-let a = (Number(annual*0.07));
-let c = (Number(annual *0.157));
-let d = annual - c
-let e = (Number(d*0.0447));
-let f = d-e
-let g = (Number(f*0.062));
-let h = f-g
-let i = (Number(h*0.0145));
-let j = h-i
-let k = j-a
+const k = 0.07;
+const f = 0.157;
+const s = 0.0447;
+const o = 0.062;
+const m = 0.0145;
+
+const salary = annual * (1-k);
+let t = f + s + o + m;
+let k = (1-t) * salary;
 
 let z = k / 24 ;
 
