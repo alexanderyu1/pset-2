b63a5dcf78849611573c20a5ce7313255f2c53f5
Alex Yu <alex@NEBULA.milkyway.net>
2019-10-27 19:32:30 -0400

    eighth commit

---
+++ src/trip.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+const students = Number(readlineSync.question("\nStudents: "));
+const teachers = Number(readlineSync.question("Teachers: "));
+const bus = Number(readlineSync.question("Bus capacity: "));
+
+let t = (students + teachers);
+let b = Math.floor((t/bus) + 1);
+
+console.log("\n" + b + " bus(es) is (are) needed, with " + (t % bus) + " passenger(s) on the last bus.");
