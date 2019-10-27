fa7462a0aab59175195493b40bfa1c3c7e177b2f
Alex Yu <alex@NEBULA.milkyway.net>
2019-10-27 19:19:05 -0400

    seventh commit

--- src/salary.js
+++ src/salary.js
@@ -1,13 +1,13 @@
 const readlineSync = require("readline-sync");
-const annual = readlineSync.question("Annual Salary: ");
+const annual = readlineSync.question("\nannual Salary: ");
 
-const k = 0.07;
+const i = 0.07;
 const f = 0.157;
 const s = 0.0447;
 const o = 0.062;
 const m = 0.0145;
 
-const salary = annual * (1-k);
+const salary = annual * (1-i);
 let t = f + s + o + m;
 let k = (1-t) * salary;
 
@@ -16,4 +16,4 @@ let z = k / 24 ;
 let r = Math.round (z);
 z = z.toLocaleString ("en", {minimumFractionDigits: 2, maximumFractionDigits: 2} );
 
-console.log("Your take-home pay each check will be $" + z + ".")
+console.log("\nYour take-home pay each check will be $" + z + ".")
